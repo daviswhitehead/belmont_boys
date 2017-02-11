72d8568f8e626dbac9c03d7a9171748d2051a198
Davis Whitehead <whitehead.davis@gmail.com>
2017-02-11 16:03:34 -0500

    tryna get django to work with heroku

--- belmont_boys/settings.py
+++ belmont_boys/settings.py
@@ -108,7 +108,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
